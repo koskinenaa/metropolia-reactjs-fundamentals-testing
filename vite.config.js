5aaa6690a59bec5d937ed7f140f7e1f7d2f14e85
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-07-23 18:03:49 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/metropolia-reactjs-fundamentals-testing/',
   plugins: [react()],
   test: {
     globals: true,
